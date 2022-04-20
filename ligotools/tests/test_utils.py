336c1e490651af56a27e9a01cb13ebbf6b1302b9
Neha Haq <nwhaq1@berkeley.edu>
2022-04-20 23:23:46 +0000

    added all 8 tests for readligo and utils

--- ligotools/tests/test_utils.py
+++ ligotools/tests/test_utils.py
@@ -1,14 +1,23 @@
-from ligotools import utils
+from ligotools.utils import whiten, write_wavfile, reqshift, plot_figures
+import numpy as np
+import os
 
 def test_utils_one():
-    
-    assert True == True
+    filepath = os.path.abspath("ligotools/tests/data/" + "strain_H1_whitenbpa.dat")
+    strain_H1_whitenbp = np.fromfile(filepath, dtype=int)
+    strain_H1_shifted = reqshift(strain_H1_whitenbp)
+
+    assert strain_H1_shifted is not None 
 
 def test_utils_two():
-    assert 0 == 0
+    assert whiten is not None
 
 def test_utils_three():
-    assert 1 == 1
+    filepath = os.path.abspath("ligotools/tests/data/" + "strain_L1_whitenbpa.dat")
+    strain_L1_whitenbp = np.fromfile(filepath, dtype=int)
+    strain_L1_shifted = reqshift(strain_L1_whitenbp)
+
+    assert strain_L1_shifted is not None 
     
 def test_utils_four():
-    assert 1 == 1
+    assert plot_figures is not None
